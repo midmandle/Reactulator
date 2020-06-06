3b0452a0dbff8726e2133af38ad638fab35e2241
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2020-06-06 17:56:47 +0100

    add test dir

---
+++ site/tests/calculator.test.tsx
@@ -0,0 +1,7 @@
+describe("calculator", () => {
+    it("should render", () => {
+        const { getByTestId } = render(<Calculator/>);
+        const calculatorRoot = getByTestId("[data-test-id='calculatorRoot']");
+        expect(calculatorRoot).toBeInTheDocument();
+    })
+})
