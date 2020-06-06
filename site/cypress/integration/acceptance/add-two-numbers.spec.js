cfd5d806a274385fbf996b44144b977a519e6cb2
Alasdair Smith <alasdairsmith0491@gmail.com>
2020-06-06 17:28:06 +0100

    Add first acceptance test add-two-numbers

--- site/cypress/integration/acceptance/add-two-numbers.spec.js
+++ site/cypress/integration/acceptance/add-two-numbers.spec.js
@@ -1,5 +1,17 @@
-describe("baklasdhjflakj", () => {
-    it("Bajfhsdaklfh", () => {
-        expect(true).toBe(false);
+import { createYield } from "typescript";
+
+describe("Given we are on the homepage", () => {
+    describe("and we can see the calculator", () => {
+        describe("When we add 1 to 1", () => {
+                it("Then it should display the result 2", () => {
+                    cy.visit("/");
+                    cy.get('#oneButton').click();
+                    cy.get('#plusButton').click();
+                    cy.get('#oneButton').click();
+                    cy.get('#equalsButton').click();
+                    cy.get('#calculationDisplay').should("contain", "1 + 1 = 2");
+                });
+            });
+        });
     });
 });
