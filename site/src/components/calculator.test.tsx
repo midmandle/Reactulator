a2c56ac05dcd2c868f2942cc2a5cf5559ae268b5
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2020-06-07 15:04:36 +0100

    refactor render function

--- site/src/components/calculator.test.tsx
+++ site/src/components/calculator.test.tsx
@@ -4,24 +4,23 @@ import React from "react";
 import Calculator from "./calculator";
 
 describe("Calculator", () => {
-    it("should render", () => {
+    beforeEach(() => {
         render(<Calculator />);
+    })
+    it("should render", () => {
         const calculatorRoot = screen.getByTestId("calculatorRoot");
         expect(calculatorRoot).toBeInTheDocument();
     });
     it("should display '1' when clicking '1'", () => {
-        render(<Calculator />);
         const number1Button = screen.getByTestId("numberOneButton");
         userEvent.click(number1Button);
         const display = screen.getByTestId("calculatorDisplay");
         expect(display).toHaveTextContent('1');
     });
     it("should display '2' when clicking '2'", () => {
-        render(<Calculator />);
         const number2Button = screen.getByTestId("numberTwoButton");
         userEvent.click(number2Button);
         const display = screen.getByTestId("calculatorDisplay");
         expect(display).toHaveTextContent('2');
-        console.log();
     });
 });
