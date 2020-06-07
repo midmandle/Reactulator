ab1b39499046c24833f55cdfffd6db8a64bf300e
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2020-06-07 15:55:21 +0100

    add button5

--- site/src/components/calculator.test.tsx
+++ site/src/components/calculator.test.tsx
@@ -16,29 +16,28 @@ describe("Calculator", () => {
         expect(calculatorRoot).toBeInTheDocument();
     });
     it("should display '1' when clicking '1'", () => {
-        const number1Button = screen.getByTestId("numberOneButton");
-        userEvent.click(number1Button);
+        const numberOneButton = screen.getByTestId("numberOneButton");
+        userEvent.click(numberOneButton);
         expect(mockCalculatorDisplayConstructor).toHaveBeenCalledWith({displayText: "1"},{});
     });
     it("should display '2' when clicking '2'", () => {
-        const number2Button = screen.getByTestId("numberTwoButton");
-        userEvent.click(number2Button);
+        const numberTwoButton = screen.getByTestId("numberTwoButton");
+        userEvent.click(numberTwoButton);
         expect(mockCalculatorDisplayConstructor).toHaveBeenCalledWith({displayText: "2"},{});
     });
     it("should display '3' when clicking '3'", () => {
-        const number3Button = screen.getByTestId("numberThreeButton");
-        userEvent.click(number3Button);
+        const numberThreeButton = screen.getByTestId("numberThreeButton");
+        userEvent.click(numberThreeButton);
         expect(mockCalculatorDisplayConstructor).toHaveBeenCalledWith({displayText: "3"},{});
     });
     it("should display '4' when clicking '4'", () => {
-        const number4Button = screen.getByTestId("numberFourButton");
-        userEvent.click(number4Button);
+        const numberFourButton = screen.getByTestId("numberFourButton");
+        userEvent.click(numberFourButton);
         expect(mockCalculatorDisplayConstructor).toHaveBeenCalledWith({displayText: "4"},{});
     });
     it("should display '5' when clicking '5'", () => {
-        const number5Button = screen.getByTestId("numberFiveButton");
-        userEvent.click(number5Button);
+        const numberFiveButton = screen.getByTestId("numberFiveButton");
+        userEvent.click(numberFiveButton);
         expect(mockCalculatorDisplayConstructor).toHaveBeenCalledWith({displayText: "5"},{});
-        console.log();
     });
 });
